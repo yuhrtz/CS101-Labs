0a96a318472431d09aec2a7c3510f666dcfc4fcf
yuhrtz <113567050+yuhrtz@users.noreply.github.com>
2022-09-14 20:46:10 -0500

    Update week 3.py

--- week 3.py
+++ week 3.py
@@ -2,46 +2,46 @@ import random
 print('Welcome to the Flarsheim Guesser!\n')
 
 def main():
-    print('Please think of a number between and including 1 and 100.')
+    print('Please think of a number between and including 1 and 100.') 
 
-    remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?'))
+    remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?')) #ask for remainder when divided by 3
 
     while remainder_3 != 0 or 1 or 2:
         if remainder_3 < 0:
             print('The value must be 0 or greater')
-            remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?'))
+            remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?')) #ask for remainder when divided by 3
         elif remainder_3 > 2:
             print('The value must be less than 3')
-            remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?'))
+            remainder_3 = int(input('\n What is the remainder when your number is divided by 3 ?')) #ask for remainder when divided by 3
         else:
             break
 
-    remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?'))
+    remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?')) #ask for remainder when divided by 3
 
     while remainder_5 != 0 or 1 or 2 or 3 or 4:
         if remainder_5 < 0:
             print('The value must be 0 or greater')
-            remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?'))
+            remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?')) #ask for remainder when divided by 5
         elif remainder_5 > 4:
             print('The value must be less than 5')
-            remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?'))
+            remainder_5 = int(input('\n What is the remainder when your number is divided by 5 ?')) #ask for remainder when divided by 5
         else:
             break
 
-    remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?'))
+    remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?')) #ask for remainder when divided by 5
 
     while remainder_7 != 0 or 1 or 2 or 3 or 4 or 5 or 6:
         if remainder_7 < 0:
             print('The value must be 0 or greater')
-            remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?'))
+            remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?')) #ask for remainder when divided by 7
         elif remainder_7 > 6:
             print('The value must be less than 7')
-            remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?'))
+            remainder_7 = int(input('\n What is the remainder when your number is divided by 7 ?')) #ask for remainder when divided by 7
         else:
             break
 
     for n in range(1, 101):
-        if n % 3 == remainder_3 and n % 5 == remainder_5 and n % 7 == remainder_7:
+        if n % 3 == remainder_3 and n % 5 == remainder_5 and n % 7 == remainder_7: #gives the number from remainder
             print(f'Your number was {n}')
             print('How amazing is that?')
 
